c8ed5f4fc1e3e2775606634085afa65137be69d1
akashagrawal9179 <akashagrawal5012@gmail.com>
2024-06-12 13:19:40 +0530

    #Added multiple routing system for all CRUD operation

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
-app.use(express.urlencoded({ extended: false }));
-
 // custom middleware logger
 app.use(logger);
 
@@ -31,53 +29,51 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+// built-in middleware to handle urlencoded data
+// in other words, form data
+// 'content-type: applicaiton/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
 // built-in middleware for json
 app.use(express.json());
 
 // serve static files
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
-app.get("^/$|index(.html)?", (req, res) => {
-  //   res.sendFile('./views/index.html', {root: __dirname});
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
+// routes
+app.use("/", require("./routes/root"));
+app.use("/subdir", require("./routes/subdir"));
+app.use("/employees", require("./routes/api/employees"));
 
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html");
-});
-
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello World!");
-  }
-);
+// app.get(
+//   "/hello(.html)?",
+//   (req, res, next) => {
+//     console.log("attempted to load hello.html");
+//     next();
+//   },
+//   (req, res) => {
+//     res.send("Hello World!");
+//   }
+// );
 
 // chaining route handlers
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
+// const one = (req, res, next) => {
+//   console.log("one");
+//   next();
+// };
 
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
+// const two = (req, res, next) => {
+//   console.log("two");
+//   next();
+// };
 
-const three = (req, res) => {
-  console.log("three");
-  res.send("Finished!");
-};
+// const three = (req, res) => {
+//   console.log("three");
+//   res.send("Finished!");
+// };
 
-app.get("/chain(.html)?", [one, two, three]);
+// app.get("/chain(.html)?", [one, two, three]);
 
 // app.use('/);
 app.all("*", (req, res) => {
